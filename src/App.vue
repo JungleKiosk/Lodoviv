cf1e5daeb58e863588557c2b67f5914084702675
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-03-10 05:47:35 +0100

    basic template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup>
+<script>
+
+
 </script>
+
 
 <template>
  
+  <h1 class="text-center mt-5" style="font-family: monospace; font-weight: bold; color: #ffbf00;">Lodoviv</h1>
+
+  <div class="container my-4 px-5">
+    <div class="row">
+      <div class="col-12 col-lg-12">
+        <div class="links">
+          <!-- use <RouterLink></RouterLink> instead <a></a> to save for each pagand not refresh_e.g. button count stay fixed!-->
+
+          <RouterLink active-class=" active" class="page_link p-2 rounded-pill" to="/">Home</RouterLink>
+          <RouterLink active-class=" active" class="page_link p-2 rounded-pill" to="/documentation">Documentazione</RouterLink>
+          <RouterLink active-class=" active" class="page_link p-2 rounded-pill" to="/exercise">Exercise</RouterLink>
+        </div>
+      </div>
+    </div>
+
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style></style>
