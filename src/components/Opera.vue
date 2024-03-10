8321dd5fc95d5f71a97229cad2f44712f165f97f
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-03-10 06:09:54 +0100

    ToggleSideMenu

---
+++ src/components/Opera.vue
@@ -0,0 +1,37 @@
+<script>
+import ToggleSideMenu from "../components/opera_root/ToggleSideMenu.vue";
+
+export default {
+    data() {
+
+    },
+    components: {
+    ToggleSideMenu
+}
+}
+
+
+</script>
+
+
+<template>
+    <div class="documentation_container">
+
+        <main class="main-content">
+
+            <header>
+
+            </header>
+
+            <section>
+                <ToggleSideMenu></ToggleSideMenu>
+                
+            </section>
+
+        </main>
+    </div>
+</template>
+  
+<style></style>
+  
+  
--- src/views/OperaView.vue
+++ src/views/OperaView.vue
@@ -1,17 +1,25 @@
-<script >
+<script>
+import Opera from '../components/Opera.vue';
+
+
+export default {
+    data() {
+
+    },
+    components: {
+        Opera
+    }
+}
 
 
 </script>
 
+
+
 <template>
-    <h1>Welcome</h1>
-    <h5>Lorem ipsum dolor sit amet.</h5>
-    <div class="col-lg-6">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis consectetur sequi voluptatum asperiores earum!
-            Reiciendis voluptates ex, voluptate distinctio minus soluta reprehenderit blanditiis quasi fugiat nulla? Ipsa
-            ullam
-            doloremque nostrum?</p>
-    </div>
+
+    <Opera></Opera>
+
 </template>
 
-<style></style>
+<style scoped></style>
