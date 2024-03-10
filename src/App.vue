4d7c9a7f7fb36c0b7ab1c66fa212a5d973cdeceb
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-03-10 21:58:55 +0100

    navbar menu

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,54 @@
 <script setup>
-
-import { RouterView, RouterLink } from "vue-router"
-
-
+import { RouterView, RouterLink } from 'vue-router';
 </script>
 
-
 <template>
-
-
-
-
   <div class="container my-4 px-5">
     <div class="row justify-content-center">
-
       <div class="col-12 col-lg-4">
         <img src="./assets/img/logo/loGoviv_sign.svg" alt="">
       </div>
+      
       <div class="col-12 col-lg-12">
-        <div class="links">
-          <!-- use <RouterLink></RouterLink> instead <a></a> to save for each pagand not refresh_e.g. button count stay fixed!-->
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <!-- Logo -->
+          <a class="navbar-brand" href="#">
+            <img src="./assets/img/logo/loGoviv_sign.svg" alt="Logo" style="max-height: 50px;">
+          </a>
 
-          <RouterLink active-class=" active" class="page_link p-2 rounded-pill" to="/">Home</RouterLink>
-          <RouterLink active-class=" active" class="page_link p-2 rounded-pill" to="/about">About me</RouterLink>
-          <RouterLink active-class=" active" class="page_link p-2 rounded-pill" to="/opera">Opera</RouterLink>
-        </div>
+          <!-- Toggle button for mobile -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <!-- Navbar links -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink active-class="active" class="nav-link txt_menu" to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink active-class="active" class="nav-link txt_menu" to="/about">About me</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink active-class="active" class="nav-link txt_menu" to="/opera">Opera</RouterLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </div>
 
+    <!-- Contenuto della pagina -->
     <RouterView />
   </div>
 </template>
 
 <style>
-
-img{
+img {
   width: 100%;
 }
 
