e3c218fbe06caeed8a6cf24fc1940b12476635b9
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-02 20:23:58 +0200

    card home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,38 +5,83 @@
 
 <template>
     <div class="container overflow-x-hidden banner_floe_home">
-        <div class="row justify-content-center">
-            <div class="col-12 col-lg-4">
-                <img class="img_face text-center" src="../assets/img/logo/loGoviv.png" alt="">
+        <div class="row justify-content-center mb-5">
+            <div class="col-12 col-lg-12 text-center">
+                <img class="img_home" src="../assets/img/logo/home.png" alt="">
             </div>
-            <div class="col-12 col-lg-8">
-                <h1>Hi! I'm Lodoviv</h1>
-                <h5>Artista Multidimensionale abitante della Rete Cosmica e Digitale.</h5>
 
-                <div class="row">
-                    <div class="col-3 col-lg-3">
-                   
-                    </div>
-                    <div class="col-3 col-lg-3">
-                    
-                    </div>
-                    <div class="col-3 col-lg-3">
-                 
+            <div class="col-12 col-lg-4">
+
+                <div class="card card_style text-left rounded-5">
+                    <img class="card-img-top" src="./../assets/img/opera/2.png" alt="">
+
+                    <div class="card-body">
+                        <h4 class="card-title text-center title_card">Antropologia</h4> <hr>
+                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
+                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
                     </div>
                 </div>
-                
+            </div>
+            <div class="col-12 col-lg-4 ">
+
+                <div class="card card_style text-left rounded-5">
+                    <img class="card-img-top" src="./../assets/img/dec/blob_otline.svg" alt="">
+
+                    <div class="card-body">
+                        <h4 class="card-title text-center title_card">Lettere</h4><hr>
+                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
+                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-4 ">
+
+                <div class="card card_style text-left rounded-5">
+                    <img class="card-img-top" src="./../assets/img/dec/blob_otline.svg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title text-center title_card">Disegno Digitale</h4><hr>
+                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
+                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
+                    </div>
+                </div>
             </div>
 
         </div>
 
     </div>
 
+    <footer id="footer" class="p-5 mt-5">
+        <div class="row mt-5">
+            <div class="col-6">
+                <h4>Contacts</h4>
+                <p>
+                    3485697226
+                </p>
+                <p>
+                    lodovico.lindemann@gmail.com</img>
+                </p>
+            </div>
+        </div>
+    </footer>
+
 
 </template>
 
 <style>
-.img_face {
-    width: 80%;
+.img_home {
+    width: 50%;
+}
+
+.card_style {
+    background-color: rgba(2, 9, 73, 0.353);
+    color: #ebbdfe;
+}
+
+.title_card{
+    color: #00fdb5;
 }
 
 /* .home_banner {
