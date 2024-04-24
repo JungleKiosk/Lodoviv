e998e28130fa1882836e93bc0f1063ad19a7904d
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-24 19:56:16 +0200

    cards home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,56 +9,74 @@
             <div class="col-12 col-lg-12 text-center">
                 <img class="img_home" src="../assets/img/logo/Hi01Artboard1.svg" alt="">
             </div>
-            <div class="col-12 col-lg-12">
+            <!--    <div class="col-12 col-lg-12">
                 <div class="cube"></div>
-            </div>
-           <!--  <div class="col-12 col-lg-12">
-                <div class="cube"></div>
-            </div> -->
-            <div class="col-12 col-lg-8 text-center">
-                <img class="img_face" src="../assets/img/logo/loGoviv.png" alt="">
-            </div>
-
-            <!-- <div class="col-12 col-lg-4">
-
-                <div class="card card_style text-left rounded-5">
-                    <img class="card-img-top" src="./../assets/img/opera/2.png" alt="">
-
-                    <div class="card-body">
-                        <h4 class="card-title text-center title_card">Antropologia</h4> <hr>
-                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
-                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-lg-4 ">
-
-                <div class="card card_style text-left rounded-5">
-                    <img class="card-img-top" src="./../assets/img/dec/blob_otline.svg" alt="">
-
-                    <div class="card-body">
-                        <h4 class="card-title text-center title_card">Lettere</h4><hr>
-                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
-                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-lg-4 ">
-
-                <div class="card card_style text-left rounded-5">
-                    <img class="card-img-top" src="./../assets/img/dec/blob_otline.svg" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title text-center title_card">Disegno Digitale</h4><hr>
-                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
-                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
-                    </div>
-                </div>
             </div> -->
 
         </div>
+
+        <div class="row justify-content-center mb-5">
+            <div class="col-12 col-lg-12 mb-5">
+                <h4>“Se stai visitando questo mio portfolio online significa che io o qualcun altro te lo ha mandato,
+                    perciò benvenut*! Che tu voglia esporre/vendere le mie opere, inaugurare una collaborazione,
+                    commissionare dei progetti grafici, formulare qualsiasi altra proposta o anche solo fare una bella
+                    chiacchierate sull’Arte non esitare a contattarmi!”</h4>
+            </div>
+        </div>
+
+        <div class="row justify-content-center my-5">
+            <div class="col-12 col-lg-4 ">
+                <div class="card card_style text-left rounded-3">
+                    <img class="card-img-top" src="./../assets/img/dec/know.png" alt="">
+
+                    <div class="card-body">
+                        <h4 class="card-title text-center title_card">News</h4>
+                        <hr>
+                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
+                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-4">
+
+                <div class="card card_style text-left rounded-3">
+                    <img class="card-img-top" src="./../assets/img/dec/Antropocen.png" alt="">
+
+                    <div class="card-body">
+                        <h4 class="card-title text-center title_card">Why?</h4>
+                        <hr>
+                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
+                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-lg-4 ">
+
+                <div class="card card_style text-left rounded-3">
+                    <img class="card-img-top" src="./../assets/img/dec/what.png" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title text-center title_card">Cyber citizenship</h4>
+                        <hr>
+                        <p class="card-text p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quibusdam tempore quas perspiciatis quidem molestiae quod, impedit quo. Earum quo numquam
+                            quasi ipsum voluptatum perferendis consequuntur commodi aspernatur natus provident!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-8 text-center">
+                <img class="img_face" src="../assets/img/logo/loGoviv.png" alt="">
+            </div>
+        </div>
+
+
+
 
     </div>
 
@@ -102,8 +120,8 @@
 }
 
 .card_style {
-    background-color: rgba(2, 9, 73, 0.353);
-    color: #ebbdfe;
+    background-color: rgba(15, 15, 15, 0.353);
+    color: #00fdb5;
 }
 
 .cube {
@@ -111,7 +129,8 @@
 }
 
 .title_card {
-    color: #00fdb5;
+    color: #ebbdfe;
+
 }
 
 #footer {
